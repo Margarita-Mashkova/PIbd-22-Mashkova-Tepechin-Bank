d90aad1b262af07ded5f9c5c388ab8b9c438adab
Маргарита Машкова <margosha.mashkova@mail.ru>
2022-04-02 03:40:17 +0400

    commit

--- Bank/Bank/Form1.Designer.cs
+++ Bank/Bank/Form1.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace BankView
+﻿namespace Bank
 {
-    partial class FormMain
+    partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,22 +23,15 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.SuspendLayout();
-            // 
-            // FormMain
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "FormMain";
-            this.Text = "Банк \"Вы банкрот\"";
-            this.ResumeLayout(false);
-
+            this.Text = "Form1";
         }
 
         #endregion
