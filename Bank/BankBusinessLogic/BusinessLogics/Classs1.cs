82a7907dcf4e83295b460fafbf41a023d38d7022
Маргарита Машкова <margosha.mashkova@mail.ru>
2022-04-02 21:12:49 +0400

    commit

---
+++ Bank/BankBusinessLogic/BusinessLogics/Classs1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankBusinessLogic.BusinessLogics
+{
+    internal class Classs1
+    {
+    }
+}
