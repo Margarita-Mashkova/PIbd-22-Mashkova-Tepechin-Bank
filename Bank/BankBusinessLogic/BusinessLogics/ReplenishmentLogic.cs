d7cdb02d8f1095676104647ed5124dabd8ac38e7
Маргарита Машкова <margosha.mashkova@mail.ru>
2022-04-02 21:46:23 +0400

    commit

---
+++ Bank/BankBusinessLogic/BusinessLogics/ReplenishmentLogic.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BankContracts.BindingModels;
+using BankContracts.BusinessLogicsContracts;
+using BankContracts.StoragesContracts;
+using BankContracts.ViewModels;
+
+namespace BankBusinessLogic.BusinessLogics
+{
+    public class ReplenishmentLogic : IReplenishmentLogic
+    {
+        private readonly IReplenishmentStorage _replenishmentStorage;
+        public ReplenishmentLogic(IReplenishmentStorage replenishmentStorage)
+        {
+            _replenishmentStorage = replenishmentStorage;
+        }
+        public List<ReplenishmentViewModel> Read(ReplenishmentBindingModel model)
+        {
+            if (model == null)
+            {
+                return _replenishmentStorage.GetFullList();
+            }
+            if (model.Id.HasValue)
+            {
+                return new List<ReplenishmentViewModel> { _replenishmentStorage.GetElement(model) };
+            }
+            return _replenishmentStorage.GetFilteredList(model);
+        }
+        public void CreateOrUpdate(ReplenishmentBindingModel model)
+        {
+            if (model.Id.HasValue)
+            {
+                _replenishmentStorage.Update(model);
+            }
+            else
+            {
+                _replenishmentStorage.Insert(model);
+            }
+        }
+        public void Delete(ReplenishmentBindingModel model)
+        {
+            var element = _replenishmentStorage.GetElement(new ReplenishmentBindingModel
+            {
+                Id = model.Id
+            });
+            if (element == null)
+            {
+                throw new Exception("Удаляемый элемент не найден");
+            }
+            _replenishmentStorage.Delete(model);
+        }
+    }
+}
