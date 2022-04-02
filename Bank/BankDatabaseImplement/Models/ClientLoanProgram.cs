5ef7efaa60e76c0492245a83ce9e8be8e4f5513b
Kirill <kir.tep18@mail.ru>
2022-04-02 22:51:25 +0400

    commit

---
+++ Bank/BankDatabaseImplement/Models/ClientLoanProgram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankDatabaseImplement.Models
+{
+    public class ClientLoanProgram
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public int LoanProgramId { get; set; }
+        [Required]
+        public int Count { get; set; }
+        public virtual Client Client { get; set; }
+        public virtual LoanProgram LoanProgram { get; set; }
+    }
+}
