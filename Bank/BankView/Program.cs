4c58de643d6d294ef61f285c6dd04e26118abd7f
Kirill <kir.tep18@mail.ru>
2022-04-02 22:51:13 +0400

    commit models

--- Bank/BankView/Program.cs
+++ Bank/BankView/Program.cs
@@ -14,7 +14,6 @@ namespace BankView
         [STAThread]
         static void Main()
         {
-
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
