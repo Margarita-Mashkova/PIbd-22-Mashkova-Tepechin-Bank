6cc518a35d3bcad495e63a2860de518949271965
Маргарита Машкова <margosha.mashkova@mail.ru>
2022-05-24 02:20:37 +0400

    word+excel web

--- Bank/BankClerkApp/Controllers/ReportController.cs
+++ Bank/BankClerkApp/Controllers/ReportController.cs
@@ -28,17 +28,47 @@ namespace BankClerkApp.Controllers
         [HttpPost]
         public IActionResult CreateReportClientCurrencyToWordFile(List<int> clientsId)
         {
-            var model = new ReportBindingModel();
-            foreach (var clientId in clientsId)
+            if (clientsId != null)
             {
-                var client = APIClerk.GetRequest<ClientViewModel>($"api/client/GetClient?clientId={clientId}");
-                model.Clients.Add(client);
+                var model = new ReportBindingModel
+                {
+                    Clients = new List<ClientViewModel>(),
+                    LoanPrograms = new List<LoanProgramViewModel>()
+                };
+                foreach (var clientId in clientsId)
+                {
+                    model.Clients.Add(APIClerk.GetRequest<ClientViewModel>($"api/client/GetClient?clientId={clientId}"));
+                }
+                model.FileName = @"..\BankClerkApp\wwwroot\ReportClientCurrency\ReportClientCurrencyDoc.doc";
+                APIClerk.PostRequest("api/report/CreateReportClientCurrencyToWordFile", model);
+                var fileName = "ReportClientCurrencyDoc.doc";
+                var filePath = _environment.WebRootPath + @"\ReportClientCurrency\" + fileName;
+                return PhysicalFile(filePath, "application/doc", fileName);
             }
-            model.FileName = @"..\BankClerkApp\wwwroot\ReportClientCurrency\ReportClientCurrencyDoc.doc";
-            APIClerk.PostRequest("api/report/CreateReportClientCurrencyToWordFile", model);
-            var fileName = "ReportClientCurrencyDoc.doc";
-            var filePath = _environment.WebRootPath + @"\ReportClientCurrency\" + fileName;
-            return PhysicalFile(filePath, "application/doc", fileName);
+            throw new Exception("Выберите хотя бы одного клиента");
+        }
+
+        [HttpPost]
+        public IActionResult CreateReportClientCurrencyToExcelFile(List<int> clientsId)
+        {
+            if (clientsId != null)
+            {
+                var model = new ReportBindingModel
+                {
+                    Clients = new List<ClientViewModel>(),
+                    LoanPrograms = new List<LoanProgramViewModel>()
+                };
+                foreach (var clientId in clientsId)
+                {
+                    model.Clients.Add(APIClerk.GetRequest<ClientViewModel>($"api/client/GetClient?clientId={clientId}"));
+                }
+                model.FileName = @"..\BankClerkApp\wwwroot\ReportClientCurrency\ReportClientCurrencyExcel.xls";
+                APIClerk.PostRequest("api/report/CreateReportClientCurrencyToExcelFile", model);
+                var fileName = "ReportClientCurrencyExcel.xls";
+                var filePath = _environment.WebRootPath + @"\ReportClientCurrency\" + fileName;
+                return PhysicalFile(filePath, "application/xls", fileName);
+            }
+            throw new Exception("Выберите хотя бы одного клиента");
         }
 
         public IActionResult ReportPDF()
